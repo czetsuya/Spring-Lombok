3c007c65e78ad8c442c05b843b839e30e1bdd7d1
Edward P. Legaspi <czetsuya@gmail.com>
2019-02-12 19:47:38 +0800

    Add internationalization.

--- src/main/java/com/broodcamp/SpringLombokApplication.java
+++ src/main/java/com/broodcamp/SpringLombokApplication.java
@@ -1,7 +1,12 @@
 package com.broodcamp;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 /**
  * @author Edward P. Legaspi
@@ -13,5 +18,11 @@ public class SpringLombokApplication {
 		SpringApplication.run(SpringLombokApplication.class, args);
 	}
 
-}
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 
+}
